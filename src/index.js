db6f2a4d15a78f9631ca4efa05dc64f9d316f9fa
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-06-07 17:42:32 -0300

    CSS - botoes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+// Aparecer alerta de como utilizar a pagina
+
+document.getElementById ("encrypt")
+
+function information(){
+    alert ("Por favor, informe no conteúdo da mensagem: \n Seu nome \n Nome e setor do autor \n Escreva números por extenso");
+} // aparecer alert ao clicar na pag criptografar
+
+// descobrir onde colocar <input type="button" onclick="information()" value="Exibir Alert" />
+
