14011a3474c475c1cf45a696d0dc04a511c07ba4
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-06-08 19:38:38 -0300

    melhoria chave secreta e CSS - juntei classes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-// Aparecer alerta de como utilizar a pagina
+/* Aparecer alerta de como utilizar a pagina
 
 document.getElementById ("encrypt")
 
@@ -10,5 +10,6 @@ function information(){
     alert ("Por favor, informe no conteúdo da mensagem: \n Seu nome \n Nome e setor do autor \n Escreva números por extenso");
 } // aparecer alert ao clicar na pag criptografar
 
-// descobrir onde colocar <input type="button" onclick="information()" value="Exibir Alert" />
+// descobrir onde colocar <input type="button" onclick="information()" value="Exibir Alert" /> */
+
 
