a578b20827ad0e60bc3ec5b9e860368aecce8f64
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-06-10 20:30:23 -0300

    index.js criar variaveis e armazenar conteudo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
+
+  /* Daniel - ((codigoDaLetraASC - codigoDaLetra + deslocamento) % tamDoAlfabeto) + codigoDaLetra 
+  string.charCodeAt(num da letra no alfabeto)
+  string.fromCharCodeAt
+
+  transformar offset em número (number ou parseInt - inteiro)
+
+    encrypt.charCodeAt(1)
+
+    funções para encriptar e desencriptar mensagem.
+
+        TESTE 
+    function encode(offset, string) {
+        let mensagem = string.charCodeAt()   // transforma letra em cod ASC (n)
+        let novoCodigo = ((mensagem - 65+ offset)%26)+ 65
+        let novaLetra = String.fromCharCode(novoCodigo) // transforma cod ASC em letra
+    
+        console.log(novoCodigo)
+      }
+    
+      encode (10, "C")
+  */
+
+  
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,40 @@ console.log(cipher);
 
 /* Aparecer alerta de como utilizar a pagina*/
 
-let info = document.getElementById ("encrypt")
-info.addEventListener("click", alerta) 
+let information = document.getElementById("mesage")
+information.addEventListener("click", warning) 
 
-function alerta(){
+function warning(){
     alert ("Por favor, informe no conteúdo da mensagem: \n Seu nome \n Nome e setor do autor \n Escreva números por extenso");
-} // aparecer alert ao clicar na pag criptografar
+} 
+
+/* Pegar as informações do campo mensagem e aparecer no campo mensagem criptografada */
+
+let string = document.querySelector(".mesage")  /* campo mensagem armazenado em string*/
+
+let offset = document.querySelector("#secret-key") /* chave secreta (deslocamento) armazenada*/
+
+let btnEncrypt = document.getElementById("encrypt") /* Ao clicar no botão criptografar irá cripto msgm */
+btnEncrypt.addEventListener("click" , encode)
+
+/* (Cipher.js) function encode() {
+
+     
+}
+
+buscar // preventDefault() */
+
+
+/* Pegar as informações do campo mensagem e aparecer no campo mensagem criptografada */
+
+let decrypto = document.querySelector("#dmesage")  /* campo mensagem cripto armazenado em decrypt*/
+
+let btnDecrypt = document.getElementById("decrypt") /* Ao clicar no botão descriptografar irá descripto msgm */
+btnDecrypt.addEventListener("click", decode)
+
+/* (Cipher.js) funtion decode() {
+
+}*/
 
 
 
